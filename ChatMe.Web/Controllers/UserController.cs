6b0135fb52036b65867702a744134452e73b850d
Uladzislau Klimiankou <vlad.klimenkoff97@gmail.com>
2016-08-23 17:46:54 +0300

    Implement dialog list page. Hope it works

--- ChatMe.Web/Controllers/UserController.cs
+++ ChatMe.Web/Controllers/UserController.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿using ChatMe.DataAccess.Entities;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
+using ChatMe.Web.Models;
 
 namespace ChatMe.Web.Controllers
 {
     public class UserController : Controller
     {
+        private AppUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+
         // GET: User
         public ActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> Messages()
+        {
+            var me = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            return View(new MessagesViewModel(me));
         }
     }
 }
---
+++ ChatMe.Web/Models/DialogViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatMe.Web.Models
+{
+    public class DialogViewModel
+    {
+        public string Author { get; set; }
+        public string LastMessageSnippet { get; set; }
+    }
+}
