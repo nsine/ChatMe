53c662b0317ab4e5f27172cea7d426df7854993c
Vlad Klimenkov <vlad.klimenkoff97@gmail.com>
2016-08-26 00:34:57 +0300

    Improve configs

--- ChatMe.DataAccess/Migrations/Configuration.cs
+++ ChatMe.DataAccess/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace ChatMe.DataAccess.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ChatMe.DataAccess.EF.ChatMeContext>
+    public sealed class Configuration : DbMigrationsConfiguration<ChatMe.DataAccess.EF.ChatMeContext>
     {
         public Configuration()
         {
